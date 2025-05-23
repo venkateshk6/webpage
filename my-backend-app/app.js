903c36e1e2ce9f32d11527bf575bdd0845040e60
venkateshk6 <venkatesh.kurnoothala9@gmail.com>
2025-05-23 16:45:42 +0530

    Delete my-backend-app directory

--- my-backend-app/app.js
+++
@@ -1,4 +0,0 @@
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => res.send('Hello from Backend!'));
-app.listen(3000, () => console.log('App running on port 3000'));
